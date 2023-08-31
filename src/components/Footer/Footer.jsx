4723e8b13f0d2b34a3253885e7b693efdaf9d5d2
DakshCodes <honeysingh617522@gmail.com>
2023-08-31 13:27:17 +0530

    complete backend and frontend

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,29 +16,25 @@ const Footer = () => {
                     <div class="footer-col">
                         <h4>company</h4>
                         <ul>
-                            <li><a href="#">about us</a></li>
-                            <li><a href="#">our services</a></li>
+                            <li><a href="employes">about us</a></li>
+                            <li><a href="#services">our services</a></li>
                             <li><a href="#">privacy policy</a></li>
-                            <li><a href="#">affiliate program</a></li>
                         </ul>
                     </div>
                     <div class="footer-col">
                         <h4>get help</h4>
                         <ul>
                             <li><a href="#">FAQ</a></li>
-                            <li><a href="#">shipping</a></li>
-                            <li><a href="#">returns</a></li>
-                            <li><a href="#">order status</a></li>
-                            <li><a href="#">payment options</a></li>
+                            <li><a href="conatct">Get Into Touch</a></li>
+                            <li><a href="pricing">Payment Plans</a></li>
                         </ul>
                     </div>
                     <div class="footer-col">
                         <h4>follow us</h4>
                         <div class="social-links">
-                            <a href="#"><i class="ri-instagram-line"></i></a>
-                            <a href="#"><i class="ri-facebook-line"></i></a>
+                            <a href="https://www.instagram.com/valuexlabs/" target={"_blank"}><i class="ri-instagram-line"></i></a>
+                            <a href="https://www.facebook.com/profile.php?id=61550782828033&is_tour_dismissed=true" target={"_blank"}><i class="ri-facebook-line"></i></a>
                             <a href="#"><i class="ri-linkedin-line"></i></a>
-                            <a href="#"><i class="ri-twitter-line"></i></a>
                         </div>
                     </div>
                 </div>
